6d32d99180aa4bf36dd7c369bd3bbd82d57c8771
Lucas <lucasbezerra1701@gmail.com>
2020-01-23 17:47:25 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React, { Component } from 'react'
+import AppContent from './components/app-content'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <AppContent />
+    )
+  }
+}
+export default App
